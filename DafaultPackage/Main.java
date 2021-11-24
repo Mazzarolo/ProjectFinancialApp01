4b67b1731ed8242d2dccf4cd49f8a431df8290aa
Mazzarolo <jdrmazzarolo@outlook.com>
2021-11-24 10:36:03 -0300

    New Commit

---
+++ DafaultPackage/Main.java
@@ -0,0 +1,50 @@
+package DafaultPackage;
+
+import org.apache.pdfbox.*;
+import org.apache.pdfbox.pdmodel.*;
+import org.apache.pdfbox.text.PDFTextStripper;
+import java.io.*;
+import java.sql.SQLOutput;
+
+public class Main {
+
+    public static void main (String[] args) {
+
+        try{
+
+            File pdfFile = new File ("src/nota.pdf");
+
+            PDDocument doc = Loader.loadPDF(pdfFile); // creating instance of pdfDoc
+
+            doc.addPage(new PDPage()); // adding page in pdf doc file
+
+            PDFTextStripper pdfStripper = new PDFTextStripper();
+
+            String text = pdfStripper.getText(doc);
+
+            System.out.println(text);
+
+            int i = text.indexOf("CNPJ");
+
+            int j = text.indexOf("Impresso");
+
+            int k = text.indexOf("CPF");
+
+            String cnpj = text.substring (i + 6, i + 24);
+
+            String data = text.substring (j + 9, j + 31);
+
+            String cpf = text.substring (k + 5, k + 19);
+
+            System.out.println("CNPJ: " + cnpj);
+
+            System.out.println("Data e Horário: " + data);
+
+            System.out.println("CPF: " + cpf);
+
+        }
+        catch(Exception e){
+            System.out.println(e.getMessage());
+        }
+    }
+}
