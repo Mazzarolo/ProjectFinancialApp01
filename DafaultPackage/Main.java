f149bf55276c1cc77ef8b482345a8cca7b69fb10
Raaulsthub <Raulsteinmetz0808@gmail.com>
2021-11-24 09:14:38 -0300

    Modified main.java

--- DafaultPackage/Main.java
+++ DafaultPackage/Main.java
@@ -1,31 +1,35 @@
-package DafaultPackage;
+package DefaultPackage;
 
-import org.apache.pdfbox.*;
 import org.apache.pdfbox.pdmodel.*;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.Loader;
 import org.apache.pdfbox.text.PDFTextStripper;
 import java.io.*;
 
+
+import java.io.IOException;
+
 public class Main {
-
     public static void main (String[] args) {
-
-        try{
-
-            File pdfFile = new File ("Impressão NFC-e.pdf");
-
-            PDDocument doc = Loader.loadPDF(pdfFile); // creating instance of pdfDoc
-
-            doc.addPage(new PDPage()); // adding page in pdf doc file
-
+        try {
+            File pdfFile = new File("/home/raul/Desktop/project/src/DefaultPackage/sample.pdf");
+            PDDocument pdDocument = Loader.loadPDF(pdfFile);
             PDFTextStripper pdfStripper = new PDFTextStripper();
-
-            String text = pdfStripper.getText(doc);
-
+            String text = pdfStripper.getText(pdDocument);
             System.out.println(text);
+            int i = text.indexOf("CNPJ");
+            int j = text.indexOf("Impresso");
+            int k = text.indexOf("CPF");
+            String s1 = text.substring(i, i + 24);
+            String s2 = text.substring(j + 8,  j + 31);
+            String s3 = text.substring(k, k + 19);
+            System.out.printf("%n%n%n%n%s", s1);
+            System.out.printf("%nData e horário: %s", s2);
+            System.out.printf("%n%s", s3);
 
-        }
-        catch(Exception e){
-            System.out.println(e.getMessage());
+
+        } catch (Exception e) {
+            e.printStackTrace();
         }
     }
 }
